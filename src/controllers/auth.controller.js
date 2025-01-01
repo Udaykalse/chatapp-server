db38c36305efe9b5237c16bc407739ae02161f82
udaykalse <udaysinhk.2000@gmail.com>
2025-01-01 17:42:17 +0530

    ./

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,19 @@
-import {generateToken} from '../lib/utlis.js';
-// import User from "../models/user.model.js";
-import User from '../models/user.model.js';
+import { generateToken } from "../lib/utlis.js";
+import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    if(!fullName || email || password){
-        return res.status(400).json({ message: "All fields are required" });
+    // if (!fullName || email || password) {
+    //   return res.status(400).json({ message: "All fields are required" });
+    // }
+
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
     if (password.length < 6) {
       return res
         .status(400)
@@ -27,30 +32,103 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
 
-    if(newUser){
-        generateToken(newUser._id,res);
-        await newUser.save();
-        return res.status(201).json({
-            _id:newUser._id,
-            fullName:newUser.fullName,
-            email:newUser.email,
-            profilePic:newUser.profilePic
-            // token:newUser.token
-            ,message: "User created successfully" });
-    }else{
-        return res.status(400).json({ message: "Invalid user data" });
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+        // token:newUser.token
+        message: "User created successfully",
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
     }
-
   } catch (error) {
-    console.log("Error in signup controller",error.message);
+    console.log("Error in signup controller", error.message);
     return res.status(500).json({ message: "Error creating user" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    generateToken(user._id, res);
+    return res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+      message: "User logged in successfully",
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("Logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (error) {
+    console.log("Error in Logout Controller ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic) {
+      return res
+        .status(400)
+        .json({ message: "Please provide profile picture" });
+    }
+
+    const uploadRes = await cloudinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadRes.secure_url },
+      { new: true }
+    );
+    res.status(200).json(updateUser);
+  } catch (error) {
+    console.log("Error in update profile controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
+export const checkAuth = (req, res) => {
+    try {
+        if (!req.user) {
+            return res.status(401).json({ message: 'User not authenticated' });
+        }
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in check auth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+// export const checkAuth = (req, res) => {
+//   try {
+//     res.status(200).json(req.user);
+//   } catch (error) {
+//     console.log("Error in checkAuth controller", error.message);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// };
+
--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,18 +1,34 @@
 import User from "../models/user.model.js";
 import Message from "./../models/message.model.js";
 
+// export const getUserForSidebar = async (req, res) => {
+//   try {
+//     const loggedInUserId = req.user._id;
+//     const user = await User.find({
+//       _id: {
+//         $ne: loggedInUserId,
+//       },
+//     }).select.apply("-password");
+//     res.status(200).json(filteredUsers);
+//   } catch (error) {
+//     console.error("Error in getUserForSideBar Controller", error.message);
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+
+
+
+
 export const getUserForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
-    const user = await User.find({
-      _id: {
-        $ne: loggedInUserId,
-      },
-    }).select.apply("-password");
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
     res.status(200).json(filteredUsers);
   } catch (error) {
-    console.error("Error in getUserForSideBar Controller", error.message);
-    res.status(500).json({ message: error.message });
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
