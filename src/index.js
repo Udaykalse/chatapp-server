ddd83143d19b94e2895985ee0d6fdd94608cf3fe
udaykalse <udaysinhk.2000@gmail.com>
2025-01-01 18:26:52 +0530

    increased Payload size in index.js to 10mb

--- src/index.js
+++ src/index.js
@@ -48,7 +48,9 @@ app.use(cors({
 }));
 
 //  Essential Middlewares
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 //  Route Handlers
